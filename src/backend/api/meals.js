a03296d24939dc6d16de0095f37ac4f0287a5b1d
malek karzoun <karzounmalek99@gmail.com>
2023-05-11 20:09:35 +0200

    add routes for meals

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,13 +2,104 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../database");
 
+// Returns all meals
+
 router.get("/", async (request, response) => {
+  const result = await knex("meal");
+
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meal").select("title");
-    response.json(titles);
+    if (!result.length) {
+      response.status(404).json("No data found");
+    } else {
+      response.status(200).json(result);
+    }
   } catch (error) {
-    throw error;
+    console.error(error);
+    response.status(500).send("Enternal server error");
+  }
+});
+
+// Adds a new meal to the database
+
+router.post("/", async (request, response) => {
+  const postData = request.body;
+  const result = await knex("meal").insert(postData);
+  try {
+    if (!postData.length) {
+      response.status(400).json("Bad request, nothing to add");
+    } else {
+      response
+        .status(201)
+        .json({ message: "New meal has been added successfully" });
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: error.message });
+  }
+});
+
+// Returns the meal by id
+
+router.get("/:id", async (request, response) => {
+  const givenId = request.params.id;
+  const result = await knex("meal").select().where("id", givenId);
+  try {
+    if (!result.length) {
+      response.json(`Meal with Id: ${givenId} is not found`);
+    } else {
+      response.json(result);
+    }
+  } catch {
+    console.log(error);
+    response.status(500).json({ error: error.message });
+  }
+});
+
+// Updates the meal by id
+
+router.put("/:id", async (request, response) => {
+  const givenId = request.params.id;
+  const newData = request.body;
+  const res = await knex("meal").select("*").where("id", givenId);
+  try {
+    // if body is empty
+    if (!Object.entries(newData).length) {
+      response.status(400).json("Bad request, nothing to update");
+    } else if (!res.length) {
+      // if there are no such id in meal-table
+      response.status(404).json(`Meal with Id: ${givenId} is not found`);
+    } else {
+      // updata the data
+      await knex("meal").select("*").where("id", givenId).update(newData);
+    }
+    // show the object after the update
+    response
+      .status(200)
+      .json(await knex("meal").select("*").where("id", givenId));
+  } catch {
+    console.log(error);
+    response.status(500).json({ error: error.message });
+  }
+});
+
+// Deletes the meal by id
+
+router.delete("/:id", async (request, response) => {
+  const givenId = request.params.id;
+  const res = await knex("meal").select("*").where("id", givenId);
+  try {
+    if (!res.length) {
+      // if there are no such id in meal-table
+      response.status(404).json(`Meal with Id: ${givenId} is not found`);
+    } else {
+      await knex("meal").where("id", givenId).del();
+      response
+        .status(200)
+        .json(`meal object with id: ${givenId} has been deleted`);
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ error: error.message });
   }
 });
 
