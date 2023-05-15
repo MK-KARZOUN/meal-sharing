41f632245bb03039f12c02513e8d9821d0a14e39
malek karzoun <karzounmalek99@gmail.com>
2023-05-15 20:53:52 +0200

    handle query parameters

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -4,20 +4,20 @@ const knex = require("../database");
 
 // Returns all meals
 
-router.get("/", async (request, response) => {
-  const result = await knex("meal");
+// router.get("/", async (request, response) => {
+//   const result = await knex("meal");
 
-  try {
-    if (!result.length) {
-      return response.status(404).json("No data found");
-    } else {
-      response.status(200).json(result);
-    }
-  } catch (error) {
-    console.error(error);
-    response.status(500).send("Enternal server error");
-  }
-});
+//   try {
+//     if (!result.length) {
+//       return response.status(404).json("No data found");
+//     } else {
+//       response.status(200).json(result);
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     response.status(500).send("Enternal server error");
+//   }
+// });
 
 // Adds a new meal to the database
 
@@ -100,6 +100,130 @@ router.delete("/:id", async (request, response) => {
     }
   } catch (error) {
     console.log(error);
+    response.status(500).json({ error: error.message });
+  }
+});
+
+// query parameters
+router.get("/", async (request, response) => {
+  const {
+    maxPrice,
+    availableReservations,
+    title,
+    dateAfter,
+    dateBefore,
+    limit,
+    sortKey,
+    sortDir,
+  } = request.query;
+
+  const isEmptyQuery = Object.keys(request.query).length === 0;
+  if (isEmptyQuery) {
+    const result = await knex("meal").select("*");
+    return response.json(result);
+  }
+
+  let res = knex("meal");
+
+  // Returns all meals that are cheaper than maxPrice.
+  if (maxPrice) {
+    debugger;
+    const parsedMaxPrice = parseInt(maxPrice);
+    if (isNaN(parsedMaxPrice)) {
+      return response
+        .status(400)
+        .json("Bad request, maxPrice value is not valid");
+    }
+    res = res.select("*").where("price", "<=", parsedMaxPrice);
+  }
+
+  // Returns all meals that still have available spots left, if true. If false, return meals that have no available spots left
+  if (availableReservations) {
+    if (availableReservations.toLowerCase() === "true") {
+      const result = await knex("meal")
+        .select(
+          "title as Meal_Name",
+          knex.raw(
+            "(max_reservations - count(reservation.id)) as Available_Spots"
+          )
+        )
+        .leftJoin("reservation", "meal.id", "reservation.meal_id")
+        .groupBy("title", "max_reservations")
+        .havingRaw("COUNT(reservation.id) < max_reservations");
+      return response.json(result);
+    } else if (availableReservations.toLowerCase() === "false") {
+      const result = await knex("meal")
+        .select("title", "price", "description")
+        .leftJoin("reservation", "meal.id", "reservation.meal_id")
+        .groupBy("title", "max_reservations", "price", "description")
+        .havingRaw("COUNT(reservation.id) >= max_reservations");
+      return response.json(result);
+    } else {
+      return response
+        .status(400)
+        .json("Bad request, availableReservations value is not valid");
+    }
+  }
+
+  // Returns all meals that partially match the given title. Rød grød will match the meal with the title Rød grød med fløde.
+  if (title) {
+    if (!title.length) {
+      return response
+        .status(400)
+        .json("Bad request, there is no value for title");
+    }
+    res = res.select("*").where("title", "like", `%${title}%`);
+  }
+
+  // Returns all meals where the date for when is after the given date.
+  if (dateAfter) {
+    if (!dateAfter.length) {
+      return response
+        .status(400)
+        .json("Bad request, there is no value for dateAfter");
+    }
+    res = res.where("when_", ">=", `${dateAfter}`);
+  }
+
+  // Returns all meals where the date for when is before the given date.
+  if (dateBefore) {
+    if (!dateBefore.length) {
+      return response
+        .status(400)
+        .json("Bad request, there is no value for dateBefore");
+    }
+    res = res.where("when_", "<=", `${dateBefore}`);
+  }
+
+  // Returns the given number of meals.
+  if (limit) {
+    const parsedLimit = parseInt(limit);
+    if (isNaN(parsedLimit)) {
+      return response.status(400).json("Bad request, limit value is not valid");
+    }
+    res = res.limit(parsedLimit);
+  }
+
+  // Returns all meals sorted by the given key. Allows when, max_reservations and price as keys. Default sorting order is asc(ending).
+  // Returns all meals sorted in the given direction. Only works combined with the sortKey and allows asc or desc.
+  if (sortKey) {
+    switch (sortKey) {
+      case "when":
+      case "max_reservations":
+      case "price":
+        res.orderBy(sortKey, sortDir || "asc");
+        break;
+      default:
+        return response
+          .status(400)
+          .json("Bad request, sortkey value is not valid");
+    }
+  }
+
+  try {
+    const finalResult = await res;
+    response.json(finalResult);
+  } catch (error) {
     response.status(500).json({ error: error.message });
   }
 });
